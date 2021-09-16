c738e8f091cddd42b016db82829be69300c13efd
Praveena-T <IT19243672@my.sliit.lk>
2021-09-16 14:48:21 +0530

    Add UI of contact me and home

---
+++ blog/blog_content/contact_me.php
@@ -0,0 +1,141 @@
+<?php
+include('../include/header.php')
+?>
+<div class="container-fluid p-5">
+
+    <h3 class="card-title mb-4 text-center">Contact Me</h3>
+
+
+
+    <!-- <div class="card-group p-3">
+        <div class="card col-6 p-2 p-2 mb-3 shadow bg-white rounded">
+            <div class="card-body mb-3">
+                <center>
+                    <h5 class="card-title mb-3 text-center  my-5">Let us get connected</h5>
+                    <section class="mb-5">
+                        <div class="container-responsive">
+                            Linkedin #3467c2 style="color: #3467c2;"
+                            <a class="btn btn-outline-primary btn-floating m-1 icon-link" href="#!" role="button"><i class="fab fa-linkedin-in"></i></a>
+                            Facebook #4267B2
+                            <a class="btn btn-outline-primary btn-floating m-1 icon-link" href="#!" role="button"><i class="fab fa-facebook-f"></i></a>
+                            Instagram  style="color: #C83D6F;"
+                            <a class="btn btn-outline-danger btn-floating m-1 icon-link" href="#!" role="button"><i class="fab fa-instagram"></i></a>
+                            Twitter style="color: #1DA1F2;"
+                            <a class="btn btn-outline-info btn-floating m-1 icon-link" href="#!" role="button"><i class="fab fa-twitter"></i></a>
+                            Github
+                            <a class="btn btn-outline-dark btn-floating m-1 icon-link" href="#!" role="button"><i class="fab fa-github"></i></a>
+                        </div>
+                    </section>
+                </center>
+            </div>
+        </div>
+        <div class="card col-6 p-2 mb-3 shadow bg-white rounded">
+            <div class="card-body">
+                <h5 class="card-title mb-4 text-center">Contact Form</h5>
+                <form>
+                    <div class="form-group row mb-2">
+                        <label for="inputEmail3" class="col-sm-3 col-form-label">Name: </label>
+                        <div class="col-sm-8">
+                            <input type="text" class="form-control field-input" id="name" name="name">
+                        </div>
+                    </div>
+                    <div class="form-group row mb-2">
+                        <label for="inputEmail3" class="col-sm-3 col-form-label">Email: </label>
+                        <div class="col-sm-8">
+                            <input type="email" class="form-control field-input" id="email" name="email">
+                        </div>
+                    </div>
+                    <div class="form-group row mb-2">
+                        <label for="inputEmail3" class="col-sm-3 col-form-label">Comment: </label>
+                        <div class="col-sm-8">
+                            <textarea type="text" class="form-control field-input" rows="5" cols="50" id="comment" name="comment"></textarea>
+                        </div>
+                    </div>
+                    <p class="card-text mt-3 text-center">
+                        <button type="submit" class="btn btn-rounded button-design" style="background-color: #FF8F5C; color:blanchedalmond">Submit</button>
+                    </p>
+                </form>
+            </div>
+        </div>
+    </div> -->
+
+
+
+
+
+
+
+
+
+    <div class="container d-flex justify-content-center">
+        <div class="card mb-3 p-2 shadow bg-white rounded" style="max-width:1000px">
+            <div class="row g-0">
+
+
+                <!-- <div class="col-5 p-2">
+                    <div class="p-2">
+                        <h5 class="card-title mb-3">Let us get connected</h5>
+
+                        <section class="mb-5">
+                            <div class="container-responsive">
+                                <li>
+                                    Linkedin #3467c2 style="color: #3467c2;"
+                <a class="btn btn-outline-primary btn-floating m-1 icon-link" href="#!" role="button"><i class="fab fa-linkedin-in"></i></a>
+                </li>
+                <li>
+                    Facebook #4267B2
+                    <a class="btn btn-outline-primary btn-floating m-1 icon-link" href="#!" role="button"><i class="fab fa-facebook-f"></i></a>
+                </li>
+                <li>
+                    Instagram  style="color: #C83D6F;"
+                    <a class="btn btn-outline-danger btn-floating m-1 icon-link" href="#!" role="button"><i class="fab fa-instagram"></i></a>
+                </li>
+                <li>
+                    Twitter style="color: #1DA1F2;"
+                    <a class="btn btn-outline-info btn-floating m-1 icon-link" href="#!" role="button"><i class="fab fa-twitter"></i></a>
+                </li>
+                <li>
+                    Github
+                    <a class="btn btn-outline-dark btn-floating m-1 icon-link" href="#!" role="button"><i class="fab fa-github"></i></a>
+                </li>
+            </div>
+            </section>
+        </div>
+    </div> -->
+
+                <div class="col">
+                    <div class="card-body">
+                        <h5 class="card-title mb-4 text-center">Contact Form</h5>
+                        <form>
+                            <div class="form-group row mb-2">
+                                <label for="inputEmail3" class="col-sm-3 col-form-label">Name: </label>
+                                <div class="col-sm-8">
+                                    <input type="text" class="form-control field-input" id="name" name="name">
+                                </div>
+                            </div>
+                            <div class="form-group row mb-2">
+                                <label for="inputEmail3" class="col-sm-3 col-form-label">Email: </label>
+                                <div class="col-sm-8">
+                                    <input type="email" class="form-control field-input" id="email" name="email">
+                                </div>
+                            </div>
+                            <div class="form-group row mb-2">
+                                <label for="inputEmail3" class="col-sm-3 col-form-label">Comment: </label>
+                                <div class="col-sm-8">
+                                    <textarea type="text" class="form-control field-input" rows="5" cols="50" id="comment" name="comment"></textarea>
+                                </div>
+                            </div>
+                            <p class="card-text mt-3 text-center">
+                                <button type="submit" class="btn btn-rounded button-design text-center" style="background-color: #FF8F5C; color:blanchedalmond">Submit</button>
+                            </p>
+                        </form>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<?php
+include('../include/footer.php')
+?>
--- blog/blog_content/home.php
+++ blog/blog_content/home.php
@@ -1,8 +1,31 @@
 <?php
 include('../include/header.php')
 ?>
-<div class="container">
 
+<div class="container">
+    <div class="col d-flex justify-content-center">
+        <div class="card mb-3 border border-dark" style="max-width: 540px;">
+            <div class="row g-0">
+                <div class="col-md-4">
+                    <div class="p-2 shadow">
+                        <img src="https://mdbootstrap.com/wp-content/uploads/2020/06/vertical.jpg" alt="..." class="img-fluid" />
+                    </div>
+                </div>
+                <div class="col-md-8">
+                    <div class="card-body">
+                        <h5 class="card-title">What is life?</h5>
+                        <p class="card-text">
+                            This is a wider card with supporting text below as a natural lead-in to
+                            additional content. This content is a little bit longer.
+                        </p>
+                        <p class="card-text">
+                            <button class="btn btn-rounded" style="background-color: #FF8F5C; color:blanchedalmond">Read</button>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
 <?php
 include('../include/footer.php')
